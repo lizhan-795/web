7d98df94f971ee7fa0d4ce439d8a1b2c23c86cbc
lizhan <603468647@qq.com>
2019-11-18 09:30:09 +0800

    班级表

---
+++ ziyun/src/main/java/com/hxzy/vo/RoleSearch.java
@@ -0,0 +1,18 @@
+package com.hxzy.vo;
+
+import com.hxzy.common.search.PageSearch;
+import lombok.Getter;
+import lombok.Setter;
+
+
+/**
+ * @author lz
+ * @data 2019-11-08
+ * @comment 描述
+ */
+@Setter
+@Getter
+public class RoleSearch extends PageSearch {
+
+    private Integer roleId;
+}
